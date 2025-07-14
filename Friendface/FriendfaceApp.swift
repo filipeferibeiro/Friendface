58b25afe600cbc2bf253a4fcb705ed7404a48614
Filipe Fernandes <filipeferibeiro@gmail.com>
2025-07-14 17:11:31 -0300

    Initial Commit

---
+++ Friendface/FriendfaceApp.swift
@@ -0,0 +1,17 @@
+//
+//  FriendfaceApp.swift
+//  Friendface
+//
+//  Created by Filipe Fernandes on 14/07/25.
+//
+
+import SwiftUI
+
+@main
+struct FriendfaceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
